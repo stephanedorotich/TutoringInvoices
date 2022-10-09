9db190dc331d9c22e90fcf43103abe8d0e9d97e4
Stephane Dorotich <stephanedorotich@gmail.com>
2022-10-08 20:54:02 -0600

    R5: Student & Session Menus functional 	- Turned ui.py into a class 	- Updated ui_operations 		- for Student section, works with student_data_class 		- for Session section, works with session_data_class

--- src/ui.py
+++ src/ui.py
@@ -2,104 +2,107 @@
 import sys, os
 import exceptions as ex
 import ui_service as use
-import ui_operations as uop
+import ui_operations
 
-# MENUS
-def mainMenu():
-	name = "MAIN MENU"
-	options = [
-		'students',
-		'sessions',
-		'invoices',
-		'analysis']
-	choice = use.menuDisplay(name, options)
-	if choice == 1:
-		studentMenu()
-	if choice == 2:
-		sessionMenu()
-	if choice == 3:
-		invoiceMenu()
-	if choice == 4:
-		analysisMenu()
+class ui_app():
+	# MENUS
+	def mainMenu(self):
+		name = "MAIN MENU"
+		options = [
+			'students',
+			'sessions',
+			'invoices',
+			'analysis']
+		choice = use.menuDisplay(name, options)
+		if choice == 1:
+			self.studentMenu()
+		if choice == 2:
+			self.sessionMenu()
+		if choice == 3:
+			self.invoiceMenu()
+		if choice == 4:
+			self.analysisMenu()
 
-def studentMenu():
-	name = "STUDENT MENU"
-	options = [
-		'Add New',
-		'View All',
-		'View Student']
-	choice = use.menuDisplay(name, options)
-	if choice == 1:
-		uop.new_student()
-	if choice == 2:
-		uop.view_all_students()
-	if choice == 3:
-		uop.view_single_student()
+	def studentMenu(self):
+		name = "STUDENT MENU"
+		options = [
+			'Add New',
+			'View All',
+			'View Student']
+		choice = use.menuDisplay(name, options)
+		if choice == 1:
+			self._uop.new_student()
+		if choice == 2:
+			self._uop.view_all_students()
+		if choice == 3:
+			self._uop.view_single_student()
 
-def sessionMenu():
-	name = "SESSION MENU"
-	options = [
-		'Add New',
-		'View All',
-		'View by Student']
-	choice = use.menuDisplay(name, options)
-	if choice == 1:
-		uop.new_session()
-	if choice == 2:
-		uop.view_all_sessions()
-	if choice == 3:
-		uop.view_sessions_by_student()
+	def sessionMenu(self):
+		name = "SESSION MENU"
+		options = [
+			'Add New',
+			'View All',
+			'View by Student']
+		choice = use.menuDisplay(name, options)
+		if choice == 1:
+			self._uop.new_session()
+		if choice == 2:
+			self._uop.view_all_sessions()
+		if choice == 3:
+			self._uop.view_sessions_by_student()
 
-def invoiceMenu():
-	name = "INVOICE MENU"
-	options = [
-		'Add New',
-		'View All',
-		'View by Student',
-		'Generate Monthly Invoices',
-		'Print Invoice by Student',
-		'Print Invoices by Month',
-		'Pay Invoice']
-	choice = use.menuDisplay(name, options)
-	if choice == 1:
-		uop.new_invoice_for_student()
-	if choice == 2:
-		uop.view_all_invoices()
-	if choice == 3:
-		uop.view_student_invoices()
-	if choice == 4:
-		uop.generate_monthly_invoices()
-	if choice == 5:
-		uop.print_student_invoice()
-	if choice == 6:
-		uop.print_monthly_invoices()
-	if choice == 7:
-		uop.pay_invoice()
+	def invoiceMenu(self):
+		name = "INVOICE MENU"
+		options = [
+			'Add New',
+			'View All',
+			'View by Student',
+			'Generate Monthly Invoices',
+			'Print Invoice by Student',
+			'Print Invoices by Month',
+			'Pay Invoice']
+		choice = use.menuDisplay(name, options)
+		if choice == 1:
+			self._uop.new_invoice_for_student()
+		if choice == 2:
+			self._uop.view_all_invoices()
+		if choice == 3:
+			self._uop.view_student_invoices()
+		if choice == 4:
+			self._uop.generate_monthly_invoices()
+		if choice == 5:
+			self._uop.print_student_invoice()
+		if choice == 6:
+			self._uop.print_monthly_invoices()
+		if choice == 7:
+			self._uop.pay_invoice()
 
-def analysisMenu():
-	name = "ANALYSIS MENU"
-	options = [
-		'Total Income',
-		'Monthly Incomes']
-	choice = use.menuDisplay(name, options)
-	if choice == 1:
-		uop.get_total_income()
-	if choice == 2:
-		uop.get_monthly_income()
+	def analysisMenu(self):
+		name = "ANALYSIS MENU"
+		options = [
+			'Total Income',
+			'Monthly Incomes']
+		choice = use.menuDisplay(name, options)
+		if choice == 1:
+			self._uop.get_total_income()
+		if choice == 2:
+			self._uop.get_monthly_income()
 
-def quit():
-	uop.save()
-	sys.exit()
+	def quit(self):
+		self._uop.save()
+		sys.exit()
 
-def run():
-	uop.load()
-	while True:
-		try:
-			mainMenu()
-		except ex.GoToMain:
-			continue
-		except ex.Quit:
-			quit()
+	def run(self):
+		self._uop = ui_operations.ui_operations()
+		self._uop.load()
+		while True:
+			try:
+				self.mainMenu()
+			except ex.GoToMain:
+				continue
+			except ex.Quit:
+				self.quit()
 
 if __name__ == "__main__":
-	run()
+	app = ui_app()
+	app.run()
